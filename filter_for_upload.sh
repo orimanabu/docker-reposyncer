6e00543049ed0627a162274aacf05cc526c53472
Manabu Ori <manabu.ori@gmail.com>
2015-08-06 12:34:19 +0900

    don't filter RHN_NAME.

--- filter_for_upload.sh
+++ filter_for_upload.sh
@@ -7,7 +7,6 @@ for file in envfile.rhel7 envfile.rhel6; do
 	-e 's/\(RHN_PASSWORD=\).*/\1mypassword/' \
 	-e 's/\(RHN_ACTIVATION_KEY=\).*/\1yyyyyyyyyyyyyyyyyyyyyyy/' \
 	-e 's/\(RHN_ORG=\).*/\1zzzzzzz/' \
-	-e 's/\(RHN_NAME=\).*/\1docker-image-reposyncer/' \
 	${file} > ${file}.filtered
 done
 
